03a6b6c0ba8aa15d981cebb44ed88c971441eb3a
sj0919 <seungjinlee09@gmail.com>
2024-12-28 20:28:56 +0900

    즐겨찾기 기능 수정

--- app/src/main/java/com/example/week1/ui/my/MyFragment.kt
+++ app/src/main/java/com/example/week1/ui/my/MyFragment.kt
@@ -7,29 +7,34 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.week1.ContactsAdapter
+import com.example.week1.R
 import com.example.week1.databinding.FragmentMyBinding
+import com.example.week1.data.Contact
 
 class MyFragment : Fragment() {
 
     private var _binding: FragmentMyBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var favoritesAdapter:ContactsAdapter
+    private lateinit var viewModel:ContactViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(MyViewModel::class.java)
+        _binding=FragmentMyBinding.inflate(inflater,container,false)
+        viewModel=ViewModelProvider(requireActivity()).get(ContactViewModel::class.java)
 
-        _binding = FragmentMyBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-
-        return root
+        viewModel.favoritesList.observe(viewLifecycleOwner){favoriteContacts->favoritesAdapter=ContactsAdapter(favoriteContacts,viewModel)
+        binding.favoritesRecyclerView.apply{
+            layoutManager=LinearLayoutManager(context)
+            adapter=favoritesAdapter
+        }}
+        return binding.root
     }
 
     override fun onDestroyView() {
